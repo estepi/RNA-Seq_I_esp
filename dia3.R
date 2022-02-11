bca6bdda17311b7d9a1c530d77a594a37c07678e
estepi <soyestepi@gmail.com>
2022-02-11 23:01:01 +0100

    try scripts and correct some typos

--- dia3.R
+++ dia3.R
@@ -1,47 +1,21 @@
 ## ----setup, include=FALSE--------------------------------------------------------------
 knitr::opts_chunk$set(echo = FALSE)
 
+library(ShortRead)
 
-## ----ShortRead, echo=TRUE, eval=FALSE--------------------------------------------------
-## library(ShortRead)
-## setwd("../data/")
-## fqQC <-qa("wt_1-short.fq")
-
-
-## ----fqQC, echo=TRUE, eval=FALSE-------------------------------------------------------
-## fqQC
-
-
-## ----report, echo=TRUE, eval=FALSE-----------------------------------------------------
-## report(fqQC, type="html", dest="fastqQAreport-wt-short")
-
-
-## ----readCount, echo=TRUE, eval=FALSE--------------------------------------------------
-## ShortRead:::.plotReadCount(fqQC)
-
-
-## ----plot2, echo=TRUE, eval=FALSE------------------------------------------------------
-## ShortRead:::.plotNucleotideCount(fqQC)
-
-
-## ----plot3, echo=TRUE, eval=FALSE------------------------------------------------------
-## df <- fqQC[["readQualityScore"]]
-## ShortRead:::.plotReadQuality(df[df$type=="read",])
-
-
-## ----plot4, echo=TRUE, eval=FALSE------------------------------------------------------
-## df <- fqQC[["sequenceDistribution"]]
-## ShortRead:::.plotReadOccurrences(df[df$type=="read",], cex=.5)
-
-
-## ----plot5, echo=TRUE, eval=FALSE------------------------------------------------------
-## ShortRead:::.freqSequences(fqQC, "read", n=1000)
-## #las primeras 1000 secuencias más representadas
-## plot(ShortRead:::.freqSequences(fqQC, "read", n=1000)$count)
-
-
-## ----plot6, echo=TRUE, eval=FALSE------------------------------------------------------
-## perCycle <- fqQC[["perCycle"]]
-## ShortRead:::.plotCycleBaseCall(perCycle$baseCall)
-## ShortRead:::.plotCycleQuality(perCycle$quality)
+setwd("../data/")
+fqQC <-qa("R1.fastq")
+fqQC
+report(fqQC, type="html", dest="fastqQAreport-wt-short")
+ShortRead:::.plotReadCount(fqQC)
+ShortRead:::.plotNucleotideCount(fqQC)
+df <- fqQC[["readQualityScore"]]
+ShortRead:::.plotReadQuality(df[df$type=="read",])
+df <- fqQC[["sequenceDistribution"]]
+ShortRead:::.plotReadOccurrences(df[df$type=="read",], cex=.5)
+ShortRead:::.freqSequences(fqQC, "read", n=1000)
+plot(ShortRead:::.freqSequences(fqQC, "read", n=1000)$count)
+perCycle <- fqQC[["perCycle"]]
+ShortRead:::.plotCycleBaseCall(perCycle$baseCall)
+ShortRead:::.plotCycleQuality(perCycle$quality)
 
